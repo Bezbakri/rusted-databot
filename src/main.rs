e238311f08e26bda5ca13b88d824d2948af55938
FPynk <acloh2@illinois.edu>
2023-04-24 16:49:46 -0500

    Merged andrew

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,14 @@ use plotly::Layout;
 use plotly::layout::Axis;
 use chrono::prelude::*;
 
-
 pub struct Data {} // User data, which is stored and accessible in all command invocations
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
 mod bez_stuff;
 mod oh_stuff;
+mod and_stuff;
 
-/// Displays your or another user's account creation date
 #[poise::command(slash_command, prefix_command)]
 async fn age(
     ctx: Context<'_>,
@@ -27,71 +26,12 @@ async fn age(
     Ok(())
 }
 
-/// Displays the member count of the server
-#[poise::command(slash_command, prefix_command)]
-async fn member_count(ctx: Context<'_>) -> Result<(), Error> {
-    let guild = ctx.guild().ok_or("Could not get guild")?;
-    let member_count = guild.member_count;
-    let response = format!("This server has {} members", member_count);
-    ctx.say(response).await?;
-    Ok(())
-}
-
-/// Graphs the number of users in the server over time
-#[poise::command(slash_command)]
-async fn user_graph(ctx: Context<'_>) -> Result<(), Error> {
-    let guild = ctx.guild().ok_or("Could not get guild")?;
-
-    // Collect member count for each day
-    let mut member_counts = HashMap::new();
-    // Create an Http reference from the Context
-    let http = ctx.as_ref().http().clone();
-    let members = guild.members(&http, Some(1000), None).await?;
-    for member in members {
-        if let Some(joined_at) = member.joined_at {
-            let date = joined_at.date().naive_utc();
-            *member_counts.entry(date).or_insert(0) += 1;
-        }
-    }
-
-    // Sort member counts by date
-    let mut member_counts: Vec<(NaiveDate, usize)> = member_counts.into_iter().collect();
-    member_counts.sort_by_key(|(date, _)| *date);
-
-    // Create plot
-    let trace = Scatter::new(
-        member_counts.iter().map(|(date, _)| date.to_string()).collect(),
-        member_counts.iter().map(|(_, count)| *count).collect(),
-    )
-    .name("Member Count")
-    .mode(plotly::common::Mode::Lines);
-
-    let mut plot = Plot::new();
-    plot.add_trace(trace);
-
-    let max_count = member_counts.iter().map(|(_, count)| count).max().unwrap_or(&0);
-
-    let layout = Layout::new()
-        .title("User Count Over Time")
-        .x_axis(Axis::new().title("Date"))
-        .y_axis(Axis::new().title("Member Count").range(vec![0., *max_count as f64]));
-
-    plot.set_layout(layout);
-
-    // Send plot
-    let image = plot.to_image("png").await?;
-    ctx.say("User count over time:", |f| f.add_file("plot.png", image.as_slice()))
-        .await?;
-
-    Ok(())
-}
-
 #[tokio::main]
 async fn main() {
     dotenv().ok();
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![age(), member_count(), bez_stuff::channel_count(), oh_stuff::server_info()],
+            commands: vec![age(), and_stuff::member_count(), bez_stuff::channel_count(), /*oh_stuff::server_info()*/],
             ..Default::default()
         })
         .token(std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN"))
--- src/oh_stuff.rs
+++ src/oh_stuff.rs
@@ -9,59 +9,59 @@ use super::{Context, Error};
 static mut QUEUE: VecDeque<serenity::Member> = VecDeque::new();
 static mut OPEN: bool = false;
 
-#[poise::command(slash_command, prefix_command)]
-pub async fn server_info(
-    ctx: Context<'_>,
-)
-    -> Result<(), Error> {
-    let guild = ctx.guild().unwrap();
-    ctx.send(|b| {
-        b.embed(|b| {
-            b.field("Members", guild.member_count, true)
-                .title(guild.name)
-        })
-            .ephemeral(false)
-    }).await?;
-    // ctx.say(response).await?;
-    Ok(())
-}
+// #[poise::command(slash_command, prefix_command)]
+// pub async fn server_info(
+//     ctx: Context<'_>,
+// )
+//     -> Result<(), Error> {
+//     let guild = ctx.guild().unwrap();
+//     ctx.send(|b| {
+//         b.embed(|b| {
+//             b.field("Members", guild.member_count, true)
+//                 .title(guild.name)
+//         })
+//             .ephemeral(false)
+//     }).await?;
+//     // ctx.say(response).await?;
+//     Ok(())
+// }
 
-#[poise::command(slash_command, prefix_command)]
-pub async unsafe fn start_OH(
-    ctx: Context<'_>,
-)
-    -> Result<(), Error> {
-    let guild = ctx.guild().unwrap();
-    // get "CA" role
-    let ta_role: serenity::Role;
-    for role in guild.roles {
-        if role.1.name == "CA" {
-            ta_role = role.1;
-        }
-    }
-    // if user has "CA" role then start OH
-    // i have no idea how to do this
-    if ctx.author().has_role().await.unwrap() {
-        let response = format!("OH has started!");
-        ctx.say(response).await?;
-        OPEN = true;
-    }
-    Ok(())
-}
+// #[poise::command(slash_command, prefix_command)]
+// pub async unsafe fn start_OH(
+//     ctx: Context<'_>,
+// )
+//     -> Result<(), Error> {
+//     let guild = ctx.guild().unwrap();
+//     // get "CA" role
+//     let ta_role: serenity::Role;
+//     for role in guild.roles {
+//         if role.1.name == "CA" {
+//             ta_role = role.1;
+//         }
+//     }
+//     // if user has "CA" role then start OH
+//     // i have no idea how to do this
+//     if ctx.author().has_role().await.unwrap() {
+//         let response = format!("OH has started!");
+//         ctx.say(response).await?;
+//         OPEN = true;
+//     }
+//     Ok(())
+// }
 
-#[poise::command(slash_command, prefix_command)]
-pub async unsafe fn print_queue(
-    ctx: Context<'_>,
-)
-    -> Result<(), Error> {
-    let guild = ctx.guild().unwrap();
-    let mut queue_str = String::new();
-    for member in &QUEUE {
-        queue_str.push_str(member.to_string().as_str());
-        queue_str += " ";
-    }
-    let response = format!("Queue: {}", queue_str);
-    ctx.say(response).await?;
-    Ok(())
-}
+// #[poise::command(slash_command, prefix_command)]
+// pub async unsafe fn print_queue(
+//     ctx: Context<'_>,
+// )
+//     -> Result<(), Error> {
+//     let guild = ctx.guild().unwrap();
+//     let mut queue_str = String::new();
+//     for member in &QUEUE {
+//         queue_str.push_str(member.to_string().as_str());
+//         queue_str += " ";
+//     }
+//     let response = format!("Queue: {}", queue_str);
+//     ctx.say(response).await?;
+//     Ok(())
+// }
 
