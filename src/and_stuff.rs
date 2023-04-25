1a3dd22eab0a7fe31abfba281b71735f7218b5d8
christopherchan <chan.zychristopher@gmail.com>
2023-04-25 11:48:00 -0500

    Fixing graph plotter

--- src/and_stuff.rs
+++ src/and_stuff.rs
@@ -4,11 +4,12 @@ use serenity::{ChannelId, GatewayIntents, Message, MessageId};
 
 use super::{Context, Error};
 
-// use plotly::{Plot, Scatter};
-// use std::collections::HashMap;
+use plotly::{Plot, Scatter};
+use std::collections::HashMap;
+use plotly::common::Mode;
 // use plotly::Layout;
 // use plotly::layout::Axis;
-// use chrono::prelude::*;
+ use chrono::prelude::*;
 
 /// Displays the member count of the server
 #[poise::command(slash_command, prefix_command)]
@@ -55,27 +56,31 @@ pub async fn count_messages(
 }
 
 // /// Graphs the number of users in the server over time
-// #[poise::command(slash_command)]
-// async fn user_graph(ctx: Context<'_>) -> Result<(), Error> {
-//     let guild = ctx.guild().ok_or("Could not get guild")?;
+#[poise::command(slash_command)]
+pub async fn user_graph(ctx: Context<'_>) -> Result<(), Error> {
+    let guild = ctx.guild().ok_or("Could not get guild")?;
 
-//     // Collect member count for each day
-//     let mut member_counts = HashMap::new();
-//     // Create an Http reference from the Context
-//     let http = ctx.as_ref().http().clone();
-//     let members = guild.members(&http, Some(1000), None).await?;
-//     for member in members {
-//         if let Some(joined_at) = member.joined_at {
-//             let date = joined_at.date().naive_utc();
-//             *member_counts.entry(date).or_insert(0) += 1;
-//         }
-//     }
+    // Collect member count for each day
+    let mut member_counts = HashMap::new();
+    // Create an Http reference from the Context
+    let http = ctx.as_ref().http().clone();
+    let members = guild.members(&http, Some(1000), None).await?;
+    for member in members {
+        if let Some(joined_at) = member.joined_at {
+            let date = joined_at.date().naive_utc();
+            *member_counts.entry(date).or_insert(0) += 1;
+        }
+    }
 
-//     // Sort member counts by date
-//     let mut member_counts: Vec<(NaiveDate, usize)> = member_counts.into_iter().collect();
-//     member_counts.sort_by_key(|(date, _)| *date);
+    // Sort member counts by date
+    let mut member_counts: Vec<(NaiveDate, usize)> = member_counts.into_iter().collect();
+    member_counts.sort_by_key(|(date, _)| *date);
 
 //     // Create plot
+    let x: Vec<NaiveDate> = member_counts.iter().map(|(x, _)| *x).collect();
+    let y: Vec<usize> = member_counts.iter().map(|(_, y)| *y).collect();
+
+    let trace = Scatter::new(x,y).mode(Mode::Lines);
 //     let trace = Scatter::new(
 //         member_counts.iter().map(|(date, _)| date.to_string()).collect(),
 //         member_counts.iter().map(|(_, count)| *count).collect(),
@@ -83,8 +88,8 @@ pub async fn count_messages(
 //     .name("Member Count")
 //     .mode(plotly::common::Mode::Lines);
 
-//     let mut plot = Plot::new();
-//     plot.add_trace(trace);
+    let mut plot = Plot::new();
+    plot.add_trace(trace);
 
 //     let max_count = member_counts.iter().map(|(_, count)| count).max().unwrap_or(&0);
 
@@ -95,10 +100,13 @@ pub async fn count_messages(
 
 //     plot.set_layout(layout);
 
+
+
+
 //     // Send plot
 //     let image = plot.to_image("png").await?;
 //     ctx.say("User count over time:", |f| f.add_file("plot.png", image.as_slice()))
 //         .await?;
 
-//     Ok(())
-// }
+    Ok(())
+}
